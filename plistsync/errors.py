eb26c62e63fde8189a6540755895422c2c1439ef
Sebastian Mohr <sebastian@mohrenclan.de>
2026-01-14 17:26:27 +0100

    Removed unused ResponseError

--- plistsync/errors.py
+++ plistsync/errors.py
@@ -1,13 +1,11 @@
 import importlib
 
 from eyconf.validation import ConfigurationError, MultiConfigurationError
-from requests import Response
 
 __all__ = [
     "ConfigurationError",
     "MultiConfigurationError",
     "NotFoundError",
-    "ResponseError",
 ]
 
 
@@ -20,18 +18,6 @@ class NotFoundError(Exception):
     def __str__(self):
         if self.resource:
             return f"{self.message}: {self.resource}"
-        return self.message
-
-
-class ResponseError(Exception):
-    def __init__(self, response: Response, message=""):
-        self.response = response
-        self.message = (
-            f"ResponseError: {message} ({response.status_code} {response.text})"
-        )
-        super().__init__(self.message)
-
-    def __str__(self):
         return self.message
 
 
