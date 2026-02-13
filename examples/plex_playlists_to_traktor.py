497414bb26546e3c7d670e4078e3c71daa556690
Sebastian Mohr <sebastian@mohrenclan.de>
2026-02-13 23:03:07 +0100

    Updated traktor examples slightly (just to get mypy happy) they example needs another look imo

--- examples/plex_playlists_to_traktor.py
+++ examples/plex_playlists_to_traktor.py
@@ -2,11 +2,13 @@ import shutil
 from datetime import datetime
 from pathlib import Path
 
+from plistsync.core.rewrite import PathRewrite
 from plistsync.logger import log
 from plistsync.services.plex.collection import (
     PlexLibrarySectionCollection,
 )
-from plistsync.services.traktor.collection import NMLPlaylistCollection
+from plistsync.services.traktor.collection import NMLCollection, NMLPlaylistCollection
+from plistsync.services.traktor.track import NMLPlaylistTrack
 
 # ---------------------------------- Options --------------------------------- #
 playlists = [
@@ -15,53 +17,62 @@ playlists = [
     "playlist3",
 ]
 plex_section_name = "Music"
-path_rewrite = "/media/music/clean:/Volumes/music/clean"
+path_rewrite = PathRewrite.from_str(
+    "/media/music/clean",
+    "/Volumes/music/clean",
+)
 nml_path = Path("./traktor_collection.nml")
 
 
 def main(
     playlists: list[str],
     nml_path: Path,
-    path_rewrite: str | None = None,
+    path_rewrite: PathRewrite | None = None,
 ):
     plex_library = PlexLibrarySectionCollection(
         plex_section_name,
     )
+    traktor_library = NMLCollection(nml_path)
+    # make a backup of the nml file just in case
+    nml_backup = nml_path.with_suffix(
+        f".{datetime.now().strftime('%Y%m%d-%H%M%S')}.bak"
+    )
+    shutil.copyfile(nml_path, nml_backup)
+
     for pl_name in playlists:
         log.info(f"\nProcessing playlist: {pl_name}")
         pl_plex = plex_library.get_playlist(name=pl_name)
         assert pl_plex is not None, "Playlist not found"
 
-        # make a backup of the nml file
-        nml_backup = nml_path.with_suffix(
-            f".{datetime.now().strftime('%Y%m%d-%H%M%S')}.bak"
-        )
-        shutil.copyfile(nml_path, nml_backup)
-
-        pl_nml = NMLPlaylistCollection(nml_path, pl_plex.name, True)
-
-        if path_rewrite:
-            old, new = path_rewrite.split(":")
-        else:
-            old = new = ""
-
-        for track in pl_plex.tracks:
-            try:
-                p = track.path
-            except Exception:
-                log.error(f"Track {track} does not have a valid path.")
-                continue
-            path_to_upsert = str(p).replace(
-                old,
-                new,
+        # Get or create playlist in traktor
+        try:
+            traktor_playlist = traktor_library.get_playlist(name=pl_plex.name)
+        except ValueError:
+            traktor_playlist = NMLPlaylistCollection(
+                traktor_library,
+                pl_plex.name,
             )
-            pl_nml.old_insert_track(Path(path_to_upsert))
+            traktor_library.upsert_playlist(traktor_playlist)
 
-        pl_nml.commit()
+        with traktor_playlist.edit():
+            for track in pl_plex.tracks:
+                try:
+                    p = track.path
+                except Exception:
+                    log.error(f"Track {track} does not have a valid path.")
+                    continue
 
-        print(f"Tracks in playlist {pl_plex.name}:")
-        for t in pl_nml.tracks:
-            print(t)
+                if not p:
+                    continue
+
+                if path_rewrite:
+                    p = path_rewrite.apply(p)
+
+                if p:
+                    traktor_playlist.tracks.append(
+                        NMLPlaylistTrack.from_path(p),
+                    )
+    traktor_library.write()
 
 
 if __name__ == "__main__":
